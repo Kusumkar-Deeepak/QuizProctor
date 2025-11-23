7b28901b60baadd3829c7de1ecb474c2086b8066
Kusumkar Deepak Prakash❤ <150149158+Kusumkar-Deeepak@users.noreply.github.com>
2025-11-23 16:24:23 +0530

    Disable LearnMore route in App.jsx
    Comment out the LearnMore route in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import AdminDashboard from './pages/AdminDashboard'; // Assuming you have this f
 import QuizComponent from './pages/QuizComponent';
 import ThankYouComponent from './pages/ThankYouComponent';
 import ViewScores from './pages/ViewScores';
-import LearnMore from './pages/LearnMore';
+// import LearnMore from './pages/LearnMore';
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
           <Route path="/quiz/:quizLink" element={<QuizComponent />} />
           <Route path="/response" element={<ThankYouComponent />} />
           <Route path="/view-scores/:quizLink" element={<ViewScores />} />
-          <Route path="/learn-more" element={<LearnMore />} />
+          {/* <Route path="/learn-more" element={<LearnMore />} /> */}
         </Routes>
       </Router>
     </AdminProvider>
